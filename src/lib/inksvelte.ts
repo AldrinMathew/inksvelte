5659e459474cdc01c2d6cdbaa80919466f425971
Aldrin Mathew <admin@aldrinmathew.com>
2022-04-06 12:55:44 +0530

    Added root file `insvelte.ts`
    - This file re-exports all components and utilities available in the
      package.

---
+++ src/lib/inksvelte.ts
@@ -0,0 +1,5 @@
+export { default as Icons } from './utilities/icons';
+export { default as Icon } from './Icon.svelte';
+export { default as IconButton } from './IconButton.svelte';
+export { default as Link } from './Link.svelte';
+export { default as ThemeMode } from './store/ThemeMode';
