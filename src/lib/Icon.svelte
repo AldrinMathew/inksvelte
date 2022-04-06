9e89f23d92fecb1404ef8d2f2342916754d5ca18
Aldrin Mathew <admin@aldrinmathew.com>
2022-04-06 10:00:00 +0530

    Added `Icon` component
    - The Icon component enables you to add vector icons to the application
      with the help of either predefined list of icons or using custom SVG
      paths
    
    - The height and width parameters are also programmatically passed to
      the final SVG element
    
    - The viewBox dimensions are expected to be present in the predefined
      SVG data or have to be provided by the user if they want a custom SVG

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Icons from './utilities/icons';
+	import { iconValues } from './utilities/icon_values';
+
+	export let name: Icons;
+	export let svg: { x: number; y: number; path: string } | undefined = undefined;
+	export let width: string = '30px';
+	export let height: string = '30px';
+	export let focusable = 'false';
+	let icon = iconValues.find((e) => e.name === name);
+</script>
+
+{#if name === Icons.custom && svg !== undefined}
+	<svg class={$$props.class} {focusable} {width} {height} viewBox="0 0 {svg.x} {svg.y}">
+		{@html svg.path}
+	</svg>
+{:else}
+	<svg class={$$props.class} {focusable} {width} {height} viewBox="0 0 {icon.x} {icon.y}">
+		{@html icon.path}
+	</svg>
+{/if}
