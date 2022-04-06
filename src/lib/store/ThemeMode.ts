7a90c1e30eda1747952f1fbd179eb8385e1d7b80
Aldrin Mathew <admin@aldrinmathew.com>
2022-04-06 09:52:40 +0530

    Added `ThemeMode`
    - ThemeMode is a store for recognising and storing values related to
      the Theme Mode of the app
    
    - It is updated whenever the the class list for `body` changes

---
+++ src/lib/store/ThemeMode.ts
@@ -0,0 +1,20 @@
+import { writable } from 'svelte/store';
+
+const isDarkMode = false;
+
+const { subscribe, set, update } = writable(isDarkMode);
+
+const change = (next: boolean) =>
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	update((_): boolean => next);
+
+const reset = () => {
+	set(isDarkMode);
+};
+
+export default {
+	isDarkMode,
+	subscribe,
+	reset,
+	change
+};
